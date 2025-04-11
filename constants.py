e21d3d626d8d144e92cca132404a50319e72847a
dnaroma <me@dna.moe>
2025-04-11 11:48:45 +0200

    fix(constants): correct path for built-in asset bundle resources

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ from anyio import Path
 
 
 UNITY_FS_CONTAINER_BASE = Path('assets/sekai/assetbundle/resources/')
-UNITY_FS_BUILT_IN_CONTAINER_BASE = Path('assets/sekai/builtinassets/resources/')
+UNITY_FS_BUILT_IN_CONTAINER_BASE = Path('assets/sekai/builtinassets/assetbundle/resources/')
